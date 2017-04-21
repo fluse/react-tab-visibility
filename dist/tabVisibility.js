4ce65cb34f5471b8ca7569b396944ed267c71e6c
Holger Schauf <holger.schauf@googlemail.com>
2017-04-21 09:11:36 +0200

    add prop-types package

--- dist/tabVisibility.js
+++ dist/tabVisibility.js
@@ -1,10 +1,18 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -20,7 +28,7 @@ var TabVisibility = function (_Component) {
     function TabVisibility(props) {
         _classCallCheck(this, TabVisibility);
 
-        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(TabVisibility).call(this, props));
+        var _this = _possibleConstructorReturn(this, (TabVisibility.__proto__ || Object.getPrototypeOf(TabVisibility)).call(this, props));
 
         _this.state = {
             tabIsVisible: true,
@@ -101,4 +109,4 @@ var TabVisibility = function (_Component) {
     return TabVisibility;
 }(_react.Component);
 
-module.exports = TabVisibility;
+exports.default = TabVisibility;
